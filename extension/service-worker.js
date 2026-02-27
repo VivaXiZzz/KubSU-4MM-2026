a98d374963b57decec7318babaa129cbf96b3998
VladimirBlinets <vladimir2004blinets@gmail.com>
2026-02-27 23:41:42 +0300

    что-то будет отправляться на бэк

--- extension/service-worker.js
+++ extension/service-worker.js
@@ -1,15 +1,27 @@
-/*function buildPayload(message) {
+const IGNORE_URL_PREFIXES = [
+  "chrome://"
+];
+
+function buildPayload(message) {
   return {
     url: message.url,
     title: message.title,
-    content: message.content
+    lang: message.lang,
+    text: message.text,
+    timestamp: new Date().toISOString()
   };
-}*/
+}
 
 chrome.runtime.onMessage.addListener(async (message) => {
   console.log("собранные данные:", message);
 
   if (!message || message.type !== "view") {
-    return;
+    return; // Пустое или неподходящее сообщение
   }
+  if (!message.url || IGNORE_URL_PREFIXES.some((prefix) => message.url.startsWith(prefix))) {
+    return; // Пустой или неподходящий URL
+  }
+
+  const payload = buildPayload(message);
+  console.log("Sending payload", payload); // TODO Send to Backend API
 });
